74dca00f242535480c009ad90f877d2b8017d805
florinpana01 <florinpana98@gmail.com>
2023-11-20 10:02:07 +0100

    Update like.controller.ts

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -15,24 +15,24 @@ export class LikeController {
 
     @EventPattern('like_request_single')
     async getOne(data) {
-        const product = await this.likeService.get(data.id);
-        return product;
+        const like = await this.likeService.get(data.id);
+        return like;
     }
 
     @EventPattern('like_created')
-    async create(product) {
-        console.log("like_created", product);
-        this.likeService.create(product);        
+    async create(like) {
+        console.log("like_created", like);
+        this.likeService.create(like);        
     }
 
     @EventPattern('like_updated')
-    async update(product) {
-        console.log("like_updated", product);
-        this.likeService.update(product.id, product)
+    async update(like) {
+        console.log("like_updated", like);
+        this.likeService.update(like.id, like)
     }
     @EventPattern('like_deleted')
     async delete(id) {
-        console.log("product_deleted", id);        
+        console.log("like_deleted", id);        
         this.likeService.delete(id);
     }
 }
